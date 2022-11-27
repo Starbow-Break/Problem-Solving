77d845ac6c4e552e74f54c422e3a74be1e89e362
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2022-11-27 17:01:04 +0900

    [Bronze IV] Title: 샤틀버스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<double, double> dd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int x, y;  cin >> x >> y;
+    if(x > y) cout << x+y;
+    else cout << y-x;
+
+    return 0;
+}
