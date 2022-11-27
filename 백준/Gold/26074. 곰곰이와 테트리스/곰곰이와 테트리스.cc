bdaee16ee8d5de8832b026dcb0ced3699af8a96c
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2022-11-27 17:04:06 +0900

    [Gold II] Title: 곰곰이와 테트리스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/26074. 곰곰이와 테트리스/곰곰이와 테트리스.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<double, double> dd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N, M; cin >> N >> M;
+    if(N*M == 2) cout << "ChongChong";
+    else cout << "GomGom";
+
+    return 0;
+}
