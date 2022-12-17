31b8c706dc5db40fa76a615548199c1cd65cec1d
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2022-12-17 22:00:46 +0900

    [Silver III] Title: 이전 순열, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int N;
+vector<int> vec;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> N;
+    if(N == 1) cout << -1;
+    else {
+        for(int i = 0; i < N; i++) { int v; cin >> v; vec.push_back(v); }
+        
+        int i; int minV = N+1;
+        for(i = N-2; i >= 0; i--) {
+            minV = min(minV, vec[i+1]);
+            if(vec[i] > vec[i+1]) break;
+        }
+        
+        if(i == -1) cout << -1;
+        else {
+            int t = 0;
+            for(int j = i+1; j < N; j++) {
+                if(vec[i] > vec[j] && t < vec[j]) {
+                    t = vec[j];
+                }
+            }
+            
+            vector<int> temp;
+            for(int j = i; j < N; j++) {
+                if(vec[j] != t) {
+                    temp.push_back(vec[j]);
+                }
+            }
+            sort(temp.rbegin(), temp.rend());
+            
+            for(int j = 0; j < i; j++) {
+                cout << vec[j] << " ";
+            }
+            cout << t << " ";
+            for(auto &te: temp) {
+                cout << te << " ";
+            }
+        }
+    }
+
+    return 0;
+}
