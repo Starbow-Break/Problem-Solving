22fcf379f1660ed330a7b9aa5ee54260434b6ed4
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2022-12-21 00:51:00 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    ll N; cin >> N;
+    if(N%4 == 1 || N%4 == 3) cout << "SK";
+    else cout << "CY";
+
+    return 0;
+}
