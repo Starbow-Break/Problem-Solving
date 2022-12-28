0c51d61bddd6cedb7fb1d14a746bd4347177e72e
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2022-12-28 16:54:03 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    string s;
+    while(getline(cin, s)) {
+        cout << s << '\n';
+    }
+
+    return 0;
+}
