bb232a7957d24dcc51b235dbbba4ac530990bdab
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-01-02 22:37:10 +0900

    [Gold V] Title: 카드 뽑기, Time: 24 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/27084. 카드 뽑기/카드 뽑기.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+#define MOD 1000000007
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int cnt[200001] = {0, };
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N; cin >> N;
+    for(int i = 1; i <= N; i++) {
+        int v; cin >> v; cnt[v]++;
+    }
+    
+    ll ans = 1;
+    for(int i = 1; i <= N; i++) {
+        ans *= (cnt[i]+1); ans %= MOD;
+    }
+    
+    cout << --ans;
+
+    return 0;
+}
