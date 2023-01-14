6032fb3fabff72e2ad0895aa87575cf4fd657c2b
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-01-15 03:51:15 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cout << 2000 << '\n';
+    for(int i = 1; i <= 999; i++) { cout << 1 << " "; }
+    for(int i = 1; i <= 1001; i++) { cout << 1000 << " "; }
+
+    return 0;
+}
