7ec7effa494156610293bb818fd1cfd41a7c6334
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-01-21 00:35:30 +0900

    [Bronze III] Title: 히스토그램, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<double, double> dd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N; cin >> N;
+    while(N--) {
+        int l; cin >> l;
+        while(l--) cout << '=';
+        cout << '\n';
+    }
+
+    return 0;
+}
