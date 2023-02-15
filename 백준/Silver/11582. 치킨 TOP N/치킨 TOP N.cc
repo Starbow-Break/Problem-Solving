eba2c39e5ee613dc178bd10c9d9183aad01f6c67
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-02-16 00:15:19 +0900

    [Silver IV] Title: 치킨 TOP N, Time: 260 ms, Memory: 8296 KB -BaekjoonHub

---
+++ 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N; cin >> N;
+    vector<int> chicken;
+    
+    for(int i = 0; i < N; i++) {
+        int v; cin >> v;
+        chicken.push_back(v);
+    }
+    
+    int k; cin >> k;
+    
+    for(int i = 0; i < N; i += N/k) {
+        sort(chicken.begin()+i, chicken.begin()+i+N/k);
+    }
+    
+    for(auto &c: chicken) cout << c << " ";
+
+    return 0;
+}
