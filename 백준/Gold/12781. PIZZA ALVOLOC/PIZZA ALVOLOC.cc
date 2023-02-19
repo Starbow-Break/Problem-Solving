f5e068a9cfc632c81f705b2c840bb20393deb733
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-02-20 00:27:33 +0900

    [Gold III] Title: PIZZA ALVOLOC, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/12781. PIZZA ALVOLOC/PIZZA ALVOLOC.cc
+++ 백준/Gold/12781. PIZZA ALVOLOC/PIZZA ALVOLOC.cc
@@ -40,10 +40,8 @@ int main()
         {arr[1].first-arr[2].first, arr[1].second-arr[2].second});
     int c3 = ccw({arr[0].first-arr[3].first, arr[0].second-arr[3].second}, 
         {arr[1].first-arr[3].first, arr[1].second-arr[3].second});
-        
-    c0 /= abs(c0); c1 /= abs(c1); c2 /= abs(c2); c3 /= abs(c3);
     
-    cout << (c0*c1 < 0 && c2*c3 < 0);
+    cout << (1LL*c0*c1 < 0 && 1LL*c2*c3 < 0);
 
     return 0;
 }
