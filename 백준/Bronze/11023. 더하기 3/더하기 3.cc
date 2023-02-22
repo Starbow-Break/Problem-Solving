62723c0867d5189fedc7b8b3e75439dc219c8b57
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-02-22 20:43:24 +0900

    [Bronze III] Title: 더하기 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11023. 더하기 3/더하기 3.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int v; int ans = 0;
+    while(cin >> v) ans += v;
+    
+    cout << ans;
+
+    return 0;
+}
