aa3021cedf240c7ff2395ef519c0afa79b1dea84
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-02-22 21:13:32 +0900

    [Bronze III] Title: 남욱이의 닭장, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11006. 남욱이의 닭장/남욱이의 닭장.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int T; cin >> T;
+    while(T--) {
+        int N, M; cin >> N >> M;
+        cout << 2*M-N << " " << N-M << '\n';
+    }
+    
+    return 0;
+}
