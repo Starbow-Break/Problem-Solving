93e9930e6e3f52c95b59a7943a6b8f25e05ce16c
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-02-22 21:44:17 +0900

    [Bronze III] Title: 서버, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10409. 서버/서버.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int n, T; cin >> n >> T;
+    int ans = 0;
+    
+    while(n--) {
+        int v; cin >> v;
+        T -= v;
+        
+        if(T < 0) break;
+        ans++;
+    }
+    
+    cout << ans;
+
+    return 0;
+}
