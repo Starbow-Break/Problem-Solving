9b965cd73389d2c24ff8d3e4bad5d10c706616f1
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-02-25 13:21:13 +0900

    [Bronze III] Title: 쿠키애호가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11134. 쿠키애호가/쿠키애호가.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<double, double> dd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int T; cin >> T;
+    while(T--) {
+        int N, C; cin >> N >> C;
+        cout << N/C+(N%C > 0) << '\n';
+    }
+
+    return 0;
+}
