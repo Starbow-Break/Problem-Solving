4aaff13a895f33e87924ea03a225115319957e11
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-02-25 14:33:35 +0900

    [Bronze III] Title: 삼각 무늬 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11320. 삼각 무늬 － 1/삼각 무늬 － 1.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<double, double> dd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int T; cin >> T;
+    while(T--) {
+        int A, B; cin >> A >> B;
+        cout << fixed << setprecision(0) << pow(A/B, 2) << '\n';
+    }
+    
+    return 0;
+}
