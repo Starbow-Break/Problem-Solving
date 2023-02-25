aaba795565a4a9bfaedf68615fbe3e9dc9b011c9
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-02-25 20:31:53 +0900

    [Bronze III] Title: Collatz Conjecture, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13236. Collatz Conjecture/Collatz Conjecture.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N; cin >> N;
+    
+    while(N > 1) {
+        cout << N << " ";
+        
+        if(N%2) N = 3*N+1;
+        else N /= 2;
+    }
+    
+    cout << 1;
+
+    return 0;
+}
