faea5dde925cd4dcf7c492d88da28b2b8722cf93
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-02-26 17:50:05 +0900

    [Bronze III] Title: Angles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15178. Angles/Angles.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int T; cin >> T;
+    while(T--) {
+        int a, b, c; cin >> a >> b >> c;
+        
+        cout << a << ' ' << b << ' ' << c << ' ' << (a+b+c == 180 ? "Seems OK" : "Check") << '\n';
+    }
+
+    return 0;
+}
