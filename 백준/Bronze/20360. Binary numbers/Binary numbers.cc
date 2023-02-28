8b5ab6479cfd10424dcc3eff287083aa6a429b1c
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-02-28 20:24:21 +0900

    [Bronze III] Title: Binary numbers, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20360. Binary numbers/Binary numbers.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N; cin >> N;
+    for(int i = 0; i <= 20; i++) {
+        if(N & (1<<i)) cout << i << ' ';
+    }
+    
+    return 0;
+}
