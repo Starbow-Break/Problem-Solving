6465e6a9c2957566ad77d73811af349a97a3f314
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-03-01 16:08:41 +0900

    [Bronze III] Title: Poniedziałki, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8794. Poniedziałki/Poniedziałki.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int T; cin >> T;
+    while(T--) {
+        int N, M, L; cin >> N >> M >> L;
+        if(L == 1) N--;
+        else N -= (M+2-L);
+        
+        cout << 1+N/M << '\n';
+    }
+
+    return 0;
+}
