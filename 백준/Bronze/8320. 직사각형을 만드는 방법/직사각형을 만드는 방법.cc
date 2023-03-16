40551ae3954b5063826cb4c593b86274f5caf3c3
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-03-16 19:26:51 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int ans = 0; int N;
+    cin >> N;
+    for(int i = 1; i <= sqrt(N); i++) {
+        ans += (N/i-i+1);
+    }
+    
+    cout << ans;
+
+    return 0;
+}
