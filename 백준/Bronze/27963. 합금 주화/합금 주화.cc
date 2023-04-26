d4481067a2560c50ab39b24ac763365c8864d09f
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-04-27 00:35:51 +0900

    [Bronze I] Title: 합금 주화, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27963. 합금 주화/합금 주화.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    double d1, d2, x;
+    cin >> d1 >> d2 >> x;
+    
+    if(d1 > d2) swap(d1, d2);
+    
+    cout << fixed << setprecision(12) << 100*d1*d2/(d2*(100-x)+d1*x);
+
+    return 0;
+}
