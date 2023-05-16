6eac922285ed31e0f1c05285b6c15fe21c3df6ff
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-05-17 02:00:03 +0900

    [Diamond V] Title: Non-Squarefree Numbers, Time: 44 ms, Memory: 2484 KB -BaekjoonHub

---
+++ 백준/Diamond/8464. Non－Squarefree Numbers/Non－Squarefree Numbers.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+vector<int> primes;
+
+bool isPrime[200000];
+void getPrimes() {
+    fill(isPrime+2, isPrime+200000, true);
+    for(int i = 2; i < 200000; i++) {
+        if(isPrime[i]) {
+            primes.push_back(i);
+            for(int j = 2*i; j < 200000; j += i) isPrime[j] = false;
+        }
+    }
+}
+
+ll numSquareFree(ll value, int cur = 1, int start = 0) {
+    ll ret = 0;
+    
+    for(int i = start; i < primes.size(); i++) {
+        int p = cur*primes[i]; 
+        if(value < 1LL*p*p) break;
+        
+        ret += value/(1LL*p*p);
+
+        if(value >= 1LL*p*primes[i+1]*p*primes[i+1])
+            ret -= numSquareFree(value, p, i+1);
+    }
+    
+    return ret;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    getPrimes();
+    
+    ll K; cin >> K;
+    
+    ll i = 1, j = 40'000'000'000;
+    while(i <= j) {
+        ll mid = (i+j)>>1;
+        ll k = numSquareFree(mid);
+        
+        if(k < K) i = mid+1;
+        else j = mid-1;
+    }
+    
+    cout << i;
+
+    return 0;
+}
