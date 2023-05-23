5d1008e755c6f2a3c41041cce2137e7f8bd95cb8
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-05-23 22:28:19 +0900

    [Diamond V] Title: 사무실 이전, Time: 984 ms, Memory: 33068 KB -BaekjoonHub

--- 백준/Diamond/16121. 사무실 이전/사무실 이전.cc
+++ 백준/Diamond/16121. 사무실 이전/사무실 이전.cc
@@ -54,8 +54,6 @@ int getCent(int cur, int prev, int size) {
 
 ll a0 = 0, a1 = 0, a2 = 0, b0 = 0, b1 = 0, b2 = 0, ans = 0;
 ll _a0 = 0, _a1 = 0, _a2 = 0, _b0 = 0, _b1 = 0, _b2 = 0;
-vector<int> temp; vector<int> visit; 
-
 void dfs(int cur, int prev = -1, int dep = 1) {
     if(isRed[cur]) {
         ans += b2; ans %= MOD;
