c0eba578cf540d7542da89bb88874830fd6908a0
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-05-24 19:43:39 +0900

    [Bronze II] Title: 2023년은 검은 토끼의 해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27494. 2023년은 검은 토끼의 해/2023년은 검은 토끼의 해.cc
+++ 백준/Bronze/27494. 2023년은 검은 토끼의 해/2023년은 검은 토끼의 해.cc
@@ -1,41 +1,62 @@
 #include <iostream>
 #include <algorithm>
-#include <string>
 #include <vector>
-#include <stack>
+#include <string>
 #include <queue>
+#include <stack>
 #include <deque>
+#include <list>
+#include <map>
 #include <cmath>
-#define MAX_V 50'000
-#define MAX_E 200'000
+#include <iomanip>
+#define MOD 1'000'000'007
 
 using namespace std;
+typedef unsigned int uint;
 typedef long long ll;
+typedef unsigned long long ull;
 typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 typedef pair<double, double> pdd;
+typedef __int128_t int128;
+
+ostream& operator<<(ostream &out, const pii &p) {
+    out << p.first << ' ' << p.second;
+    return out;
+}
+
+ll powTen[19]; //pow10[i] = 10^i
+int arr[4] = {2, 0, 2, 3};
+
+ll solve(ll value, int p, int idx = 0) {
+    if(idx == 4) return value+1;
+    if(p < 0 && idx < 4) return 0;
+    
+    int q = value/powTen[p];
+    if(q < arr[idx]) {
+        return (solve(powTen[p]-1, p-1, idx)*q+solve(value-powTen[p]*q, p-1, idx))%MOD;
+    }
+    else if(q > arr[idx]) {
+        return (solve(powTen[p]-1, p-1, idx)*(q-1)+solve(value-powTen[p]*q, p-1, idx)+solve(powTen[p]-1, p-1, idx+1))%MOD;
+    }
+    else {
+        return (solve(powTen[p]-1, p-1, idx)*q+solve(value-powTen[p]*q, p-1, idx+1))%MOD;
+    }
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
-    int N, ans = 0;
-    cin >> N;
+    powTen[0] = 1;
+    for(int i = 1; i <= 18; i++) { powTen[i] = powTen[i-1]*10; }
     
-    int arr[4] = {2, 0, 2, 3};
+    ll N; cin >> N;
+    int p = 18;
+    while(powTen[p] > N) p--;
     
-    for(int i = 1; i <= N; i++) {
-        int idx = 3; int cur = i;
-        
-        while(cur > 0 && idx >= 0) {
-            if(cur%10 == arr[idx]) idx--;
-            cur /= 10;
-        }
-        
-        ans += (idx == -1);
-    }
-    
-    cout << ans;
+    cout << solve(N, p);
 
     return 0;
 }
