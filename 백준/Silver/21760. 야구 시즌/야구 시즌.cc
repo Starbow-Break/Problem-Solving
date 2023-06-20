e2f9bf9220d67d40fbe9ab9a78bfda8f9a12a20d
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-06-20 15:36:13 +0900

    [Silver V] Title: 야구 시즌, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/21760. 야구 시즌/야구 시즌.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef unsigned int uint;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef __int128_t int128;
+
+ostream& operator<<(ostream &out, const pii &p) {
+    out << p.first << ' ' << p.second;
+    return out;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int T; cin >> T;
+    while(T--) {
+        int N, M, k, D; cin >> N >> M >> k >> D;
+        int t = M*(M-1)/2*N*k+M*M*N*(N-1)/2;
+        
+        int B = D/t;
+        int ans = B*t;
+        
+        cout << (ans ? ans : -1) << '\n';
+    }
+
+    return 0;
+}
