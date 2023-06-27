ba0fce9011cb6c012f5914184ef5f4165888a984
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-06-28 01:42:17 +0900

    [Silver I] Title: 케이크(?) 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14445. 케이크（？） 자르기/케이크（？） 자르기.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef unsigned int uint;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef __int128_t int128;
+
+ostream& operator<<(ostream &out, const pii &p) {
+    out << p.first << ' ' << p.second;
+    return out;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    ll N; cin >> N;
+    if(N == 1) cout << 0;
+    else if(N == 2) cout << 1;
+    else cout << N/2+(N%2>0);
+    
+    return 0;
+}
