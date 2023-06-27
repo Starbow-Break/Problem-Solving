ff28d27d08620e5f767b359310ad2808b629c1c2
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-06-27 15:32:58 +0900

    [Silver IV] Title: 카드놀이, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/3231. 카드놀이/카드놀이.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef unsigned int uint;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef __int128_t int128;
+
+ostream& operator<<(ostream &out, const pii &p) {
+    out << p.first << ' ' << p.second;
+    return out;
+}
+
+int idx[100'001];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N; cin >> N;
+    for(int i = 1; i <= N; i++) {
+        int v; cin >> v;
+        idx[v] = i;
+    }
+    
+    int ans = 0;
+    for(int i = 2; i <= N; i++) {
+        ans += (idx[i-1] > idx[i]);
+    }
+    
+    cout << ans;
+
+    return 0;
+}
