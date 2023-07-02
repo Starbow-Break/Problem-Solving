86f82570dcc4b6826bb180cb9a05d922120a362d
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-07-02 16:54:20 +0900

    [Silver IV] Title: 일차함수들, Time: 32 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/25426. 일차함수들/일차함수들.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef unsigned int uint;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef __int128_t int128;
+
+ostream& operator<<(ostream &out, const pii &p) {
+    out << p.first << ' ' << p.second;
+    return out;
+}
+
+int a[100'000], b[100'000];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N; cin >> N;
+    for(int i = 0; i < N; i++) cin >> a[i] >> b[i];
+    sort(a, a+N);
+    
+    ll ans = 0;
+    for(int i = 1; i <= N; i++) {
+        ans += 1LL*a[i-1]*i+b[i-1];
+    }
+    cout << ans;
+
+    return 0;
+}
