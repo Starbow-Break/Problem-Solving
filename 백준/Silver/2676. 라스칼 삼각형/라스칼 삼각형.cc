2ecbb5c609c7646e24fede0f47dfdeab3f0ed2d1
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-07-11 08:54:57 +0900

    [Silver II] Title: 라스칼 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2676. 라스칼 삼각형/라스칼 삼각형.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef __int128_t int128;
+
+ostream& operator<<(ostream &out, const pii &p) {
+    out << p.first << ' ' << p.second << '\n';
+    return out;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int T; cin >> T;
+    while(T--) {
+        int n, m; cin >> n >> m;
+        cout << 1+1LL*m*(n-m) << '\n';
+    }
+    
+    return 0;
+}
