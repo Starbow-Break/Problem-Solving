0f1a0d4771e059c35a29a0d1cbe37be7f4af7316
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-07-22 19:35:39 +0900

    [Diamond IV] Title: 조개 줍기, Time: 452 ms, Memory: 43640 KB -BaekjoonHub

--- 백준/Diamond/14870. 조개 줍기/조개 줍기.cc
+++ 백준/Diamond/14870. 조개 줍기/조개 줍기.cc
@@ -17,58 +17,40 @@ ostream& operator<<(ostream &out, const pii &p) {
     return out;
 }
 
-//세그먼트 트리
-struct segmentTree {
-    ll sgTree[1<<12];
-    ll lazy[1<<12];
+//펜윅 트리
+struct fenwickTree {
+    ll _fenwick[1<<11];
     
-    segmentTree() {
-        fill(sgTree, sgTree+(1<<12), 0);
-        fill(lazy, lazy+(1<<12), 0);
+    fenwickTree() {
+        fill(_fenwick, _fenwick+(1<<11), 0);
     }
     
-    //노드 갱신
-    void propagate(int s, int e, int idx) {
-        if(s != e) {
-            lazy[idx<<1] += lazy[idx];
-            lazy[idx<<1|1] += lazy[idx];
+    ll sum(int s) {
+        ll ret = 0;
+        while(s > 0) {
+            ret += _fenwick[s];
+            s -= (s & -s);
         }
-        sgTree[idx] += lazy[idx]*(e-s+1);
-        lazy[idx] = 0;
+        return ret;
     }
     
-    //업데이트
-    void update(int s, int e, int idx, int i, int j, int del) {
-        propagate(s, e, idx);
-        
-        if(e < i || j < s) return;
-        if(i <= s && e <= j) {
-            lazy[idx] += del;
-            propagate(s, e, idx);
-            return;
+    void add(int idx, int del) {
+        while(idx < (1<<11)) {
+            _fenwick[idx] += del;
+            idx += (idx & -idx);
         }
-        
-        int mid = s+e>>1;
-        update(s, mid, idx<<1, i, j, del); update(mid+1, e, idx<<1|1, i, j, del);
-        sgTree[idx] = sgTree[idx<<1]+sgTree[idx<<1|1];
     }
     
-    //합 쿼리
-    ll query(int s, int e, int idx, int i, int j) {
-        propagate(s, e, idx);
-        
-        if(e < i || j < s) return 0;
-        if(i <= s && e <= j) return sgTree[idx];
-        
-        int mid = s+e>>1;
-        return query(s, mid, idx<<1, i, j)+query(mid+1, e, idx<<1|1, i, j);
+    //Range update
+    void update(int s, int e, int del) {
+        add(s, del); add(e+1, -del);
     }
     
-    //세그트리가 관리하는 요소들의 합계
-    ll total() {
-        return sgTree[1];
+    //point query
+    ll query(int s) {
+        return sum(s);
     }
-}segTree[1501];
+}fenwick[1501];
 
 int arr[1501][1501] = {0, };
 int dp[1501][1501] = {0, };
@@ -79,6 +61,7 @@ int main()
     cin.tie(NULL); cout.tie(NULL);
     
     int N; cin >> N;
+    ll ans = 0;
     for(int i = 1; i <= N; i++) {
         for(int j = 1; j <= N; j++) cin >> arr[i][j];
     }
@@ -86,22 +69,19 @@ int main()
     for(int i = 1; i <= N; i++) {
         for(int j = 1; j <= N; j++) {
             dp[i][j] = max(dp[i-1][j], dp[i][j-1])+arr[i][j];
+            ans += dp[i][j];
         }
     }
     
-    //dp에 맞춰 세그먼트 트리 최기화
+    //dp에 맞춰 펜윅 트리 초기화
     for(int i = 1; i <= N; i++) {
         for(int j = 1; j <= N; j++) {
-            segTree[i].update(0, N, 1, j, j, dp[i][j]);
+            fenwick[i].update(j, j, dp[i][j]);
         }
     }
     
-    //처음 답 구하기
-    ll firstAns = 0;
-    for(int i = 1; i <= N; i++) {
-        firstAns += segTree[i].total();
-    }
-    cout << firstAns << '\n';
+    //처음 답 출력
+    cout << ans << '\n';
     
     for(int i = 1; i <= N; i++) {
         char q; int r, c; cin >> q >> r >> c;
@@ -113,20 +93,21 @@ int main()
             while(t <= N) {
                 while(t > r && a <= b) { //a 이동
                     //cout << t << ' ' << a << ' ' << b << '\n';
-                    ll temp1 = segTree[t].query(0, N, 1, a-1, a-1);
-                    ll temp2 = segTree[t-1].query(0, N, 1, a, a);
+                    ll temp1 = fenwick[t].query(a-1);
+                    ll temp2 = fenwick[t-1].query(a);
                     if(temp1 >= temp2) a++;
                     else break;
                 }
                 if(a > b) break; //a가 b를 넘어서면 해당 행 부터 갱신이 이루어지지 않으므로 중단
                 while(b < N) { //b 이동
-                    ll temp1 = segTree[t-1].query(0, N, 1, b+1, b+1);
-                    ll temp2 = segTree[t].query(0, N, 1, b, b);
+                    ll temp1 = fenwick[t-1].query(b+1);
+                    ll temp2 = fenwick[t].query(b);
                     if(temp1 < temp2+1) b++;
                     else break;
                 }
                 //[a, b]구간 업데이트
-                segTree[t].update(0, N, 1, a, b, 1);
+                fenwick[t].update(a, b, 1);
+                ans += (b-a+1);
                 t++; //다음 행으로 이동
             }
         }
@@ -135,19 +116,20 @@ int main()
             int a = c, b = c, t = r;
             while(t <= N) {
                 while(t > r && a <= b) {
-                    ll temp1 = segTree[t].query(0, N, 1, a-1, a-1);
-                    ll temp2 = segTree[t-1].query(0, N, 1, a, a);
+                    ll temp1 = fenwick[t].query(a-1);
+                    ll temp2 = fenwick[t-1].query(a);
                     if(temp1 > temp2) a++;
                     else break;
                 }
                 if(a > b) break;
                 while(b < N) {
-                    ll temp1 = segTree[t-1].query(0, N, 1, b+1, b+1);
-                    ll temp2 = segTree[t].query(0, N, 1, b, b);
+                    ll temp1 = fenwick[t-1].query(b+1);
+                    ll temp2 = fenwick[t].query(b);
                     if(temp1 <= temp2-1) b++;
                     else break;
                 }
-                segTree[t].update(0, N, 1, a, b, -1);
+                fenwick[t].update(a, b, -1);
+                ans -= (b-a+1);
                 t++;
             }
         }
@@ -160,10 +142,6 @@ int main()
         // }
         
         //구하고자 하는 값 출력
-        ll ans = 0;
-        for(int i = 1; i <= N; i++) {
-            ans += segTree[i].total();
-        }
         cout << ans << '\n';
     }
 
