0fbe9ce6b1e46d63015691f444c670b796b2f4df
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-08-10 08:12:30 +0900

    [Gold II] Title: 유클리드 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/4342. 유클리드 게임/유클리드 게임.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define repeat(n) for(int i = 0; i < n; i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define pb push_back 
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define compress(vec) sort(all(vec)); vec.erase(unique(all(vec)), vec.end());
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+using int128 = __int128_t;
+
+vector<int> vec;
+void solve(int a, int b) {
+    if(a < b) swap(a, b);
+    if(!(a%b)) return;
+    
+    vec.pb(a/b);
+    solve(b, a%b);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int a, b;
+    while(cin >> a >> b) {
+        if(!a && !b) break;
+        vec.clear();
+        solve(a, b);
+        int cnt = 0;
+        for(auto &v: vec) {
+            if(v != 1) break;
+            cnt++;
+        }
+        
+        cout << (cnt%2 ? "B" : "A") << " wins" << '\n';
+    }
+    
+    return 0;
+}
