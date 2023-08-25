06006f5a109d770b2f27704484752dd40d4fe3c8
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-08-25 10:21:29 +0900

    [Gold IV] Title: 단어 암기, Time: 976 ms, Memory: 2316 KB -BaekjoonHub

---
+++ 백준/Gold/18119. 단어 암기/단어 암기.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define repeat(n) for(int i = 0; i < n; i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define pb push_back 
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define compress(vec) sort(all(vec)); vec.erase(unique(all(vec)), vec.end());
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+using int128 = __int128_t;
+
+bool used[10000][26] = {false, };
+int cnt[10000] = {0, };
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N, M; cin >> N >> M;
+    FOR(i, 0, N) {
+        string s; cin >> s;
+        for(auto &c: s) {
+            used[i][c-'a'] = true;
+        }
+    }
+    
+    int ans = N;
+    while(M--) {
+        int q; char c; cin >> q >> c;
+        if(q == 1) {
+            for(int i = 0; i < N; i++) {
+                if(used[i][c-'a']) {
+                    if(!cnt[i]) ans--;
+                    cnt[i]++;
+                }
+            }
+        }
+        if(q == 2) {
+            for(int i = 0; i < N; i++) {
+                if(used[i][c-'a']) {
+                    cnt[i]--;
+                    if(!cnt[i]) ans++;
+                }
+            }
+        }
+        
+        cout << ans << '\n';
+    }
+    
+    return 0;
+}
