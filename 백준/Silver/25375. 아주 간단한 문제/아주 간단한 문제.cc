2fd0751e564cf6395eb43633b511da557d12238b
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-08-28 11:50:52 +0900

    [Silver V] Title: 아주 간단한 문제, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25375. 아주 간단한 문제/아주 간단한 문제.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define repeat(n) for(int i = 0; i < n; i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define pb push_back 
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define compress(vec) sort(all(vec)); vec.erase(unique(all(vec)), vec.end());
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+using int128 = __int128_t;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int T; cin >> T;
+    while(T--) {
+        ll a, b; cin >> a >> b;
+        if(b < 2*a) cout << 0;
+        else if(b%a) cout << 0;
+        else cout << 1;
+        cout << '\n';
+    }
+    
+    return 0;
+}
