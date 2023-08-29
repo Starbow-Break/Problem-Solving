1e0deeb24575bede71c390a02d39b10313e155e6
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-08-29 09:35:34 +0900

    [Gold V] Title: 크리보드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11058. 크리보드/크리보드.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define repeat(n) for(int i = 0; i < n; i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define pb push_back 
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define compress(vec) sort(all(vec)); vec.erase(unique(all(vec)), vec.end());
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+using int128 = __int128_t;
+
+ll dp[101] = {0, };
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N; cin >> N;
+    for(int i = 0; i <= N; i++) {
+        if(i+1 <= N) dp[i+1] = max(dp[i+1], dp[i]+1);
+        for(int j = 3; j <= N-i; j++) {
+            dp[i+j] = max(dp[i+j], dp[i]*(j-1));
+        }
+    }
+    
+    cout << dp[N];
+    
+    return 0;
+}
