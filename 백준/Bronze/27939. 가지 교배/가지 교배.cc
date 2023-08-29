1489f523f0330d22bbef467a0c3cb832ddfa65c2
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-08-29 16:31:38 +0900

    [Bronze I] Title: 가지 교배, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27939. 가지 교배/가지 교배.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define repeat(n) for(int i = 0; i < n; i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define pb push_back 
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define compress(vec) sort(all(vec)); vec.erase(unique(all(vec)), vec.end());
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+using int128 = __int128_t;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N; cin >> N;
+    char gaji[1001];
+    for(int i = 1; i <= N; i++) {
+        cin >> gaji[i];
+    }
+    
+    int m, k; cin >> m >> k;
+    bool white = false;
+    for(int i = 1; i <= m; i++) {
+        bool w = true;
+        for(int j = 1; j <= k; j++) {
+            int v; cin >> v;
+            if(gaji[v] == 'P') w = false;
+        }
+        
+        if(w) white = true;
+    }
+    
+    cout << (white ? "W" : "P");
+    
+    return 0;
+}
