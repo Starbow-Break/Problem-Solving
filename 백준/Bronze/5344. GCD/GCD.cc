77976ac247fbf1bc5adf60ceb8670d63fe08d871
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-08-29 16:34:11 +0900

    [Bronze I] Title: GCD, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5344. GCD/GCD.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define repeat(n) for(int i = 0; i < n; i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define pb push_back 
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define compress(vec) sort(all(vec)); vec.erase(unique(all(vec)), vec.end());
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+using int128 = __int128_t;
+
+int gcd(int a, int b) {
+    if(a < b) swap(a, b);
+    return (b ? gcd(b, a%b) : a);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N; cin >> N;
+    while(N--) {
+        int a, b; cin >> a >> b;
+        cout << gcd(a, b) << '\n';
+    }
+    
+    return 0;
+}
