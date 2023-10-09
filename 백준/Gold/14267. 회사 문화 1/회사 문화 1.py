1b43489e975958fc1f66d7fff817ef3e7b9ef748
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-10-09 22:00:06 +0900

    [Gold IV] Title: 회사 문화 1, Time: 216 ms, Memory: 43396 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
@@ -1,18 +1,20 @@
 import sys
-# sys.stdin = open('input.txt')
+#sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
+# 받은 칭찬의 총합
 dp = [0 for _ in range(N+1)]
+# 직속 상사
 parent = [0]
 parent.extend(list(map(int, input().split())))
 
-edges = [[] for _ in range(N+1)]
-
+# 직접 받은 칭찬 반영
 for _ in range(M):
     i, w = map(int, input().split())
     dp[i] += w
 
+# dp[i] = dp[parent[i]] + (직접 받은 칭찬)
 for i in range(1, N+1):
     if parent[i] != -1:
         dp[i] += dp[parent[i]]
