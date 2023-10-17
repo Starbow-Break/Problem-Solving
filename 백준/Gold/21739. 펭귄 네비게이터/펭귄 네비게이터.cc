e12e769c84a74eaf91207fff0b8c9d9992d1a9df
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-10-17 14:50:13 +0900

    [Gold II] Title: 펭귄 네비게이터, Time: 540 ms, Memory: 783388 KB -BaekjoonHub

---
+++ 백준/Gold/21739. 펭귄 네비게이터/펭귄 네비게이터.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define repeat(n) for(int i = 0; i < n; i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define pb push_back 
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define compress(vec) sort(all(vec)); vec.erase(unique(all(vec)), vec.end());
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+using int128 = __int128_t;
+
+const int MOD = 1'000'000'007;
+int dp[20001][10001];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int N; cin >> N;
+    dp[0][0] = 1;
+    for(int i = 1; i <= 2*N; i++) {
+        for(int j = 0; j <= min(i, 2*N-i); j++) {
+            if(j+1 <= 10000) dp[i][j] += dp[i-1][j+1];
+            if(j-1 >= 0) dp[i][j] += dp[i-1][j-1];
+            dp[i][j] %= MOD;
+        }
+    }
+    
+    cout << dp[2*N][0];
+    
+    return 0;
+}
