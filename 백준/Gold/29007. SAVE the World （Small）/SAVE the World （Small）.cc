f2a8abb6bac8b767c4b7c1919e0def9f33ba06a3
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2023-10-20 15:15:01 +0900

    [Gold IV] Title: SAVE the World (Small), Time: 0 ms, Memory: 2420 KB, Score: 50000000 point -BaekjoonHub

--- 백준/Gold/29007. SAVE the World （Small）/SAVE the World （Small）.cc
+++ 백준/Gold/29007. SAVE the World （Small）/SAVE the World （Small）.cc
@@ -20,55 +20,48 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
-    int N; cin >> N;
-    vector<pii> pos; //용사들의 위치
-    for(int i = 1; i <= N; i++) {
+    string s = "DXAW";
+    string t = "";
+    for(int i = 1; i <= 50; i++) {
+        for(int j = 2*(i-1); j < 2*i; j++) {
+            for(int k = 0; k < i; k++) {
+                t += s[j%4];
+            }
+        }
+    }
+    
+    int n; cin >> n;
+    vector<pii> pos;
+    
+    for(int i = 0; i < n; i++) {
         int x, y; cin >> x >> y;
         pos.pb({x, y});
     }
     
-    pii delta = {0, 0}; //위치 보정
-    vector<string> route(N); //경로
-    bool finished[500] = {false, }; //도착 여부
-    
-    int cnt = 0;
-    string t = "";
-    while(cnt < N) {
-        int target = -1;
-        for(int i = 0; i < N; i++) {
-            if(!finished[i]) {
-                if(target == -1) target = i;
-                else {
-                    if(abs(pos[target].fi+delta.fi)+abs(pos[target].se+delta.se)
-                    > abs(pos[i].fi+delta.fi)+abs(pos[i].se+delta.se)) {
-                        target = i;
-                    }
-                }
+    string cur = "";
+    vector<string> ans(n);
+    int cnt = 0, idx = 0;
+    int x = 0, y = 0;
+    while(cnt < n) {
+        cur += t[idx];
+        char c = t[idx];
+        if(c == 'D') x--;
+        if(c == 'A') x++;
+        if(c == 'W') y--;
+        if(c == 'X') y++;
+        
+        for(int i = 0; i < n; i++) {
+            if(pos[i] == make_pair(x, y)) {
+                ans[i] = cur;
+                cnt++;
+                break;
             }
         }
         
-        int dx = -(pos[target].fi+delta.fi);
-        int dy = -(pos[target].se+delta.se);
-        delta.fi += dx; delta.se += dy;
-        while(dx > 0) {
-            t += 'D'; dx--;
-        }
-        while(dx < 0) {
-            t += 'A'; dx++;
-        }
-        while(dy > 0) {
-            t += 'W'; dy--;
-        }
-        while(dy < 0) {
-            t += 'X'; dy++;
-        }
-        
-        route[target] = t;
-        finished[target] = true;
-        cnt++;
+        idx++;
     }
     
-    for(int i = 0; i < N; i++) cout << route[i] << '\n';
+    for(int i = 0; i < n; i++) cout << ans[i] << '\n';
     
     return 0;
 }
