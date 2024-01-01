88481e44684f9bd667db39eb83bd16d79e314ea4
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2024-01-01 22:01:17 +0900

    [Silver V] Title: 마트료시카 합치기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/25631. 마트료시카 합치기/마트료시카 합치기.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define repeat(n) for(int i = 0; i < n; i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define pb push_back 
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define compress(vec) sort(all(vec)); vec.erase(unique(all(vec)), vec.end());
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+using int128 = __int128_t;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    map<int, int> cnt;
+    int ans = 0;
+    
+    int N; cin >> N;
+    while(N--) {
+        int v; cin >> v;
+        auto t = cnt.find(v);
+        
+        if(t == cnt.end()) {
+            cnt.insert({v, 1});
+            ans = max(ans, 1);
+        }
+        else {
+            t->se++;
+            ans = max(ans, t->se);
+        }
+    }
+    
+    cout << ans;
+    
+    return 0;
+}
