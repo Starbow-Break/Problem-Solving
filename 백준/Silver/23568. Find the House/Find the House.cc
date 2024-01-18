dffc0ae4b4d667b84f1f2f9931f8d6d1cea58914
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2024-01-19 08:43:54 +0900

    [Silver V] Title: Find the House, Time: 12 ms, Memory: 17524 KB -BaekjoonHub

---
+++ 백준/Silver/23568. Find the House/Find the House.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define repeat(n) for(int i = 0; i < n; i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define pb push_back 
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define compress(vec) sort(all(vec)); vec.erase(unique(all(vec)), vec.end());
+    
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+using int128 = __int128_t;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N; cin >> N;
+    
+    int d[2'000'001], w[2'000'001];
+    for(int i = 0; i < N; i++) {
+        int s, ww; char dd; cin >> s >> dd >> ww;
+        d[s+1'000'000] = (dd == 'R')*2-1;
+        w[s+1'000'000] = ww;
+    }
+    
+    int cur, cnt = 0; cin >> cur;
+    
+    while(cnt < N) {
+        if(cur < -1'000'000 || cur > 1'000'000) break;
+        cur += d[cur+1'000'000]*w[cur+1'000'000];
+        cnt++;
+    }
+    
+    cout << cur;
+    
+    return 0;
+}
