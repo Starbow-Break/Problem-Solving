ab027a3cf09ae2be6d0298074d895b0b2f646a9c
STARBOW <103927296+Starbow-Break@users.noreply.github.com>
2024-03-26 09:00:00 +0900

    [Silver IV] Title: 떨어진 수정, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13170. 떨어진 수정/떨어진 수정.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define repeat(n) for(int i = 0; i < n; i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define pb push_back 
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define compress(vec) sort(all(vec)); vec.erase(unique(all(vec)), vec.end());
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+using int128 = __int128_t;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N, K, P, W; cin >> N >> K >> P >> W;
+    cout << P/W+(P%W > 0);
+    
+    return 0;
+}
